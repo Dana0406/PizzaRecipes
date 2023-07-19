6baea1f953fa18db5ef1048b2d4fd3a2bec71dbf
Dana0406 <gildana33@gmail.com>
2023-07-19 12:47:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pizzarecipes/Utils.java
@@ -0,0 +1,28 @@
+package com.example.pizzarecipes;
+
+public class Utils {
+    public static final String RECIPE = "Good day, cooks! I have already written " +
+            "recipes for pizza in the oven for you, as well as a way to prepare a yeast-free dough for it.\n" +
+            "Today we are moving away from the classics and traditions and cooking a quick pizza in a frying pan!\n" +
+            "This is a very good way to cook a quick dinner (or breakfast) when there is no time to stand at the stove " +
+            "for a long time, but you want to eat delicious. More often, the dough for such an emergency pizza is " +
+            "made liquid and poured into a frying pan. Then the filling is laid out and everything is baked for about 10 minutes.\n" +
+            "The most popular recipe for batter made of sour cream and mayonnaise. " +
+            "But you can do without these products, and I'll tell you how to do it. There is another option for " +
+            "those who eat right or just watch their figure. And for fans of unusual shapes, you can make " +
+            "pancakes or hot pizza-style sandwiches."+
+            "recipes for pizza in the oven for you, as well as a way to prepare a yeast-free dough for it.\n" +
+            "Today we are moving away from the classics and traditions and cooking a quick pizza in a frying pan!\n" +
+            "This is a very good way to cook a quick dinner (or breakfast) when there is no time to stand at the stove " +
+            "for a long time, but you want to eat delicious. More often, the dough for such an emergency pizza is " +
+            "made liquid and poured into a frying pan. Then the filling is laid out and everything is baked for about 10 minutes.\n" +
+            "The most popular recipe for batter made of sour cream and mayonnaise. " +
+            "But you can do without these products, and I'll tell you how to do it. There is another option for " +
+            "those who eat right or just watch their figure. And for fans of unusual shapes, you can make " +
+            "pancakes or hot pizza-style sandwiches.";
+    public static final String PEPERONI_DESCRIPTION = "This pizza makes your day better";
+    public static final String CHEESE_DESCRIPTION = "This pizza is a base on pizza menu";
+    public static final String GREECE_DESCRIPTION = "This pizza makes your day brighter";
+    public static final String VILLAGE_DESCRIPTION = "This pizza will remind you of your childhood";
+    public static final String MEXICO_DESCRIPTION = "This pizza adds to your day bright colors";
+}
